7503dfda90a907608dd85e407a69b967440e581c
Danielsio <102610047+Danielsio@users.noreply.github.com>
2022-05-07 20:57:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randSrc1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randSrc1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randSrc2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randSrc2);
+
+if (randomNumber1 === randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+else if (randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+}
